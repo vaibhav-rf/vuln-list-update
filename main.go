ef25682e275cdd7c481eaaf7d226fef18621b301
Vaibhav Thatai <vaibhav@rapidfort.com>
2026-02-25 11:29:19 +0530

    refactor(main.go): minor formatting changes to argument definitions, no functional changes

--- main.go
+++ main.go
@@ -42,9 +42,9 @@ var (
 	target = flag.String("target", "", "update target (nvd, alpine, alpine-unfixed, redhat, redhat-oval, "+
 		"redhat-csaf-vex, debian, ubuntu, amazon, oracle-oval, suse-cvrf, photon, arch-linux, glad, cwe, osvdev, mariner, kevc, wolfi, "+
 		"chainguard, azure, openeuler, echo, minimos, eoldates, rootio, rapidfort)")
-	vulnListDir        = flag.String("vuln-list-dir", "", "vuln-list dir")
-	targetUri          = flag.String("target-uri", "", "alternative repository URI (only glad)")
-	targetBranch       = flag.String("target-branch", "", "alternative repository branch (only glad)")
+	vulnListDir  = flag.String("vuln-list-dir", "", "vuln-list dir")
+	targetUri    = flag.String("target-uri", "", "alternative repository URI (only glad)")
+	targetBranch = flag.String("target-branch", "", "alternative repository branch (only glad)")
 )
 
 func main() {
